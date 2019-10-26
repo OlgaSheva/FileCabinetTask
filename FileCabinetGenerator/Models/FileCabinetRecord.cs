196166ba491dd35a91ee2e361e49fd5c5aff3f66
Olga Kripulevich <olga.kripulevich@gmail.com>
2019-10-26 12:30:20 +0300

    Implement the FileCabinetRecords Generator.

---
+++ FileCabinetGenerator/Models/FileCabinetRecord.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace FileCabinetGenerator
+{
+    /// <summary>
+    /// The class to subscribe file cabinet record.
+    /// </summary>
+    public class FileCabinetRecord
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetRecord"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="dateOfBirth">The date of birth.</param>
+        /// <param name="gender">The gender.</param>
+        /// <param name="office">The office.</param>
+        /// <param name="salary">The salary.</param>
+        public FileCabinetRecord(int id, string firstName, string lastName, DateTime dateOfBirth, char gender, short office, decimal salary)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DateOfBirth = dateOfBirth;
+            this.Gender = gender;
+            this.Office = office;
+            this.Salary = salary;
+        }
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of birth.
+        /// </summary>
+        /// <value>
+        /// The date of birth.
+        /// </value>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the marital status.
+        /// </summary>
+        /// <value>
+        /// The marital status.
+        /// </value>
+        public char Gender { get; set; } // 'M' - male, 'F' - female, 'O' - other, 'U' - unknown
+
+        /// <summary>
+        /// Gets or sets the cats count.
+        /// </summary>
+        /// <value>
+        /// The cats count.
+        /// </value>
+        public short Office { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cats budget.
+        /// </summary>
+        /// <value>
+        /// The cats budget.
+        /// </value>
+        public decimal Salary { get; set; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.Append($"{this.Id}, ");
+            builder.Append($"{this.FirstName}, ");
+            builder.Append($"{this.LastName}, ");
+            builder.Append($"{this.DateOfBirth.ToString("yyyy-MMM-dd", CultureInfo.InvariantCulture)}, ");
+            builder.Append($"{this.Gender}, ");
+            builder.Append($"{this.Office}, ");
+            builder.Append($"{this.Salary}");
+            return builder.ToString();
+        }
+    }
+}
--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using CommandLine;
+
 using FileCabinetGenerator.CommandLineOptions;
 
 namespace FileCabinetGenerator
 {
-    class Program
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
         {
             string outputType = "csv";
             string output = "records.csv";
@@ -22,8 +31,43 @@ namespace FileCabinetGenerator
                        recordAmount = o.RecordAmount;
                        startId = o.StartId;
                    });
+            List<FileCabinetRecord> fileCabinetRecords = FileCabinetRecordGenerator(startId, 10);
+            foreach (var item in fileCabinetRecords)
+            {
+                Console.WriteLine(item.ToString());
+            }
 
-            Console.WriteLine($"{recordAmount} records were written to {output}.");
+            Console.ReadKey();
+        }
+
+        private static List<FileCabinetRecord> FileCabinetRecordGenerator(int startId, int recordAmoutn)
+        {
+            var recordsList = new List<FileCabinetRecord>(recordAmoutn);
+            Random random = new Random();
+            string genderChars = "MFOU";
+            DateTime minDate = new DateTime(1950, 1, 1);
+            DateTime maxDate = new DateTime(2005, 1, 1);
+            int daysDiff = Convert.ToInt32(maxDate.Subtract(minDate).TotalDays + 1);
+
+            for (int i = 0; i < recordAmoutn; i++)
+            {
+                FileCabinetRecord record = new FileCabinetRecord(
+                    startId + i,
+                    "fn" + random.Next(0, 10000),
+                    "ln" + random.Next(0, 10000),
+                    minDate.AddDays(random.Next(0, daysDiff)),
+                    genderChars[random.Next(0, genderChars.Length - 1)],
+                    (short)random.Next(0, 500),
+                    (decimal)NextDouble(random, 0, 4000));
+                recordsList.Add(record);
+            }
+
+            return recordsList;
+        }
+
+        private static double NextDouble(Random rnd, double min, double max)
+        {
+            return Math.Round((rnd.NextDouble() * (max - min)) + min, 2);
         }
     }
 }
